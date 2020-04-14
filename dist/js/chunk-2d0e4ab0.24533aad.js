a0eddf425fb74d16e351502e9a3f39f896738e54
xiaochou <1538786904@qq.com>
2020-04-14 19:49:21 +0800

    打包

---
+++ dist/js/chunk-2d0e4ab0.24533aad.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e4ab0"],{"90cf":function(e,n,t){"use strict";t.r(n);var u=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("router-view")],1)},c=[],r=t("e90a"),a={},l=Object(r["a"])(a,u,c,!1,null,null,null);n["default"]=l.exports}}]);
